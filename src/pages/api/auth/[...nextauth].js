cb3177bc44defedc2232872e4f0611c52564d343
Divyam2600 <ag.divyam12@gmail.com>
2022-06-08 02:10:26 +0530

    authentication

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider  from "next-auth/providers/google"
 
 export default NextAuth({
-  providers: [
+  providers:[
    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
